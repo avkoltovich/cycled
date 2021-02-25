a83501e825a192440a6af9ef5648f55f898722fe
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-02-25 22:28:11 +0300

    feat: добавлено меню

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+  styleUrls: [ './main-menu.component.scss' ]
 })
 export class MainMenuComponent implements OnInit {
+  public isMenuOpen = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  public onClickMenuButton(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
