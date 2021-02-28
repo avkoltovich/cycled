b3597fed73bf96b051a29add26a4ad3b26fdfc41
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-02-28 17:26:03 +0300

    feat: добавлена функция получения строки даты списка тренировок

--- src/app/components/workout-card-list/workout-card-list.component.ts
+++ src/app/components/workout-card-list/workout-card-list.component.ts
@@ -6,6 +6,11 @@ export interface WorkoutCardList {
   workouts: WorkoutCard[]
 }
 
+interface WorkoutListDate {
+  dayOfWeek: string,
+  date: string
+}
+
 @Component({
   selector: 'app-workout-card-list',
   templateUrl: './workout-card-list.component.html',
@@ -16,10 +21,21 @@ export class WorkoutCardListComponent implements OnInit {
   @Input()
   public workoutList: WorkoutCardList
 
+  public workoutListDate: WorkoutListDate
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.workoutListDate = this.getWorkoutListDate(this.workoutList.date)
   }
 
+  public getWorkoutListDate(dateISO: string): WorkoutListDate {
+    const date = new Date(dateISO)
+
+    return {
+      dayOfWeek: new Intl.DateTimeFormat('ru-RU', { weekday: 'long' }).format(date),
+      date: new Intl.DateTimeFormat('ru-RU', { day: 'numeric', month: 'long' }).format(date)
+    }
+  }
 }
