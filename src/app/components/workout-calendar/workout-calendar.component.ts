e1261d296fd419fc47e9e9dbd37261969d5009ea
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-02-28 22:32:20 +0300

    feat: добавлены дополнительные моки

--- src/app/components/workout-calendar/workout-calendar.component.ts
+++ src/app/components/workout-calendar/workout-calendar.component.ts
@@ -3,7 +3,7 @@ import { WorkoutCardList } from '../workout-card-list/workout-card-list.componen
 
 const mockWorkoutCalendar: WorkoutCardList[] = [
   {
-    date: '2021-08-28T07:42:09Z',
+    date: '2021-03-07T07:42:09Z',
     workouts: [
       {
         workoutType: 'rc',
@@ -17,20 +17,20 @@ const mockWorkoutCalendar: WorkoutCardList[] = [
       {
         workoutType: 'ed',
         date: '2021-08-28T05:15:09Z',
-        routePoints: [ 'Катюша', 'Кореновск' ],
+        routePoints: [ 'Тахтамукай', 'Калужская' ],
         oneWayRoute: false,
-        distance: 100,
-        speed: 30,
-        bikeType: 'Шоссе',
+        distance: 60,
+        speed: 25,
+        bikeType: 'CX',
       },
       {
         workoutType: 'tp',
         date: '2021-08-28T05:42:09Z',
-        routePoints: [ 'Яблоновский', 'Шенджий' ],
+        routePoints: [ 'Смоленская', 'Крепостная' ],
         oneWayRoute: false,
-        distance: 100,
-        speed: 30,
-        bikeType: 'Шоссе',
+        distance: 50,
+        speed: 20,
+        bikeType: 'MTB',
       },
       {
         workoutType: 'tr',
@@ -46,7 +46,48 @@ const mockWorkoutCalendar: WorkoutCardList[] = [
         date: '2021-08-28T07:30:09Z',
         routePoints: [ 'Пашковский Хлебзавод', 'Горячий Ключ', 'Пашковский Хлебзавод' ],
         oneWayRoute: false,
+        distance: 100,
+        speed: 25,
+        bikeType: 'Шоссе',
+      }
+    ]
+  },
+  {
+    date: '2021-03-08T07:42:09Z',
+    workouts: [
+      {
+        workoutType: 'an',
+        date: '2021-08-28T05:15:09Z',
+        routePoints: [ 'Тахтамукай', 'Калужская' ],
+        oneWayRoute: false,
+        distance: 60,
+        speed: 25,
+        bikeType: 'CX',
+      },
+      {
+        workoutType: 'tp',
+        date: '2021-08-28T05:42:09Z',
+        routePoints: [ 'Смоленская', 'Крепостная' ],
+        oneWayRoute: false,
         distance: 50,
+        speed: 20,
+        bikeType: 'MTB',
+      },
+      {
+        workoutType: 'tr',
+        date: '2021-08-28T06:00:09Z',
+        routePoints: [ 'ДБ Екатерининский', 'Мачуги' ],
+        oneWayRoute: true,
+        distance: 50,
+        speed: 25,
+        bikeType: 'Шоссе',
+      },
+      {
+        workoutType: 'vo',
+        date: '2021-08-28T07:30:09Z',
+        routePoints: [ 'Пашковский Хлебзавод', 'Горячий Ключ', 'Пашковский Хлебзавод' ],
+        oneWayRoute: false,
+        distance: 100,
         speed: 25,
         bikeType: 'Шоссе',
       }
