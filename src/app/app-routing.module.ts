8f11dfc225f97fbc87e7ab9dc31e466ac6da5607
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-03-01 23:20:28 +0300

    feat: добавлен роут списка карточек тренировок

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkoutListPageComponent } from './components/workouts-list-page/workout-list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WorkoutListPageComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,13 @@ import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { WorkoutCardComponent } from './workouts-list-page/components/workout-card/workout-card.component';
 import { MatCardModule } from '@angular/material/card';
-import { WorkoutCardListComponent } from './workouts-list-page/components/workout-card-list/workout-card-list.component';
-import { PowerZonesDialogComponent } from './workouts-list-page/dialogs/power-zones-dialog/power-zones-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { AddWorkoutButtonComponent } from './workouts-list-page/components/add-workout-button/add-workout-button.component';
-import { WorkoutListPageComponent } from './workouts-list-page/workout-list-page.component';
+import { WorkoutCardComponent } from './components/workouts-list-page/components/workout-card/workout-card.component';
+import { WorkoutCardListComponent } from './components/workouts-list-page/components/workout-card-list/workout-card-list.component';
+import { PowerZonesDialogComponent } from './components/workouts-list-page/dialogs/power-zones-dialog/power-zones-dialog.component';
+import { AddWorkoutButtonComponent } from './components/workouts-list-page/components/add-workout-button/add-workout-button.component';
+import { WorkoutListPageComponent } from './components/workouts-list-page/workout-list-page.component';
 
 @NgModule({
   declarations: [
