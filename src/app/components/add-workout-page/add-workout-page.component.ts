024294f42fa900e4c797812a320df577a7549b0c
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-03-03 00:47:46 +0300

    feat: добавлен блок информации на последнем шаге создания тренировки

--- src/app/components/add-workout-page/add-workout-page.component.ts
+++ src/app/components/add-workout-page/add-workout-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { tap } from 'rxjs/operators';
+import { WorkoutCard } from '../workouts-list-page/models/models';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-add-workout-page',
@@ -12,6 +14,19 @@ export class AddWorkoutPageComponent implements OnInit {
   public routeFormGroup: FormGroup;
   public bikeTypeFormGroup: FormGroup;
   public detailsFormGroup: FormGroup;
+  /**
+   * TODO Сделать проверку в компоненте с блоком информации по тренировке на отсутствие данных, чтобы не мокать ничего заранее
+   */
+  public workout: WorkoutCard = {
+    workoutType: 'rc',
+    date: new Date().toISOString(),
+    routePoints: [ '' ],
+    oneWayRoute: false,
+    distance: 0,
+    speed: 0,
+    bikeType: 'Любой',
+    members: [],
+  };
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -22,7 +37,7 @@ export class AddWorkoutPageComponent implements OnInit {
       to: [ null, Validators.required ]
     });
     this.bikeTypeFormGroup = this.formBuilder.group({
-      type: [ 'any' ]
+      bikeType: [ 'any' ]
     });
     this.detailsFormGroup = this.formBuilder.group({
       speed: [ null, Validators.required ],
@@ -30,16 +45,26 @@ export class AddWorkoutPageComponent implements OnInit {
       duration: [ null ]
     });
 
-    this.bikeTypeFormGroup.valueChanges.pipe(
-      tap((value) => {
-        console.log(value);
+    combineLatest([
+      this.routeFormGroup.valueChanges,
+      this.bikeTypeFormGroup.valueChanges,
+      this.detailsFormGroup.valueChanges
+    ]).pipe(
+      tap(([ route, bikeType, details ]) => {
+        this.workout = {
+          workoutType: 'rc',
+          date: new Date().toISOString(),
+          routePoints: [ route.from, route.to ],
+          oneWayRoute: false,
+          distance: details.distance,
+          speed: details.speed,
+          bikeType,
+          members: [],
+        };
       })
     ).subscribe();
   }
 
   public onSubmit(): void {
-    console.log('routeFormGroup', this.routeFormGroup.value);
-    console.log('bikeTypeFormGroup', this.bikeTypeFormGroup.value);
-    console.log('detailsFormGroup', this.detailsFormGroup.value);
   }
 }
