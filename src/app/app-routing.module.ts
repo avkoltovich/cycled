36c0951916d51f3e1f625626f936f5539dd1bfd8
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-03-03 19:40:07 +0300

    wip: отключает роут добавления тренировки

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WorkoutListPageComponent } from './components/workouts-list-page/workout-list-page.component';
-import { AddWorkoutPageComponent } from './components/add-workout-page/add-workout-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WorkoutListPageComponent
   },
-  {
-    path: 'add-workout',
-    component: AddWorkoutPageComponent
-  }
+  // {
+  //   path: 'add-workout',
+  //   component: AddWorkoutPageComponent
+  // }
 ];
 
 @NgModule({
