d02f3174d0b11354745604d2fe1435eea2c8d747
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-03-03 22:23:57 +0300

    feat: стилизация и валидация страницы создания тренировки

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { WorkoutListPageComponent } from './components/workouts-list-page/workout-list-page.component';
+import { WorkoutListPageComponent } from './components/workouts-list-page/workout-list-page.component'
+import { AddWorkoutPageComponent } from './components/add-workout-page/add-workout-page.component'
 
 const routes: Routes = [
   {
     path: '',
     component: WorkoutListPageComponent
   },
-  // {
-  //   path: 'add-workout',
-  //   component: AddWorkoutPageComponent
-  // }
-];
+  {
+    path: 'add-workout',
+    component: AddWorkoutPageComponent
+  }
+]
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }) ],
--- src/app/components/add-workout-page/add-workout-page.component.ts
+++ src/app/components/add-workout-page/add-workout-page.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { tap } from 'rxjs/operators';
-import { WorkoutCard } from '../workouts-list-page/models/models';
-import { combineLatest } from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { tap } from 'rxjs/operators'
+import { WorkoutCard } from '../workouts-list-page/models/models'
+import { combineLatest } from 'rxjs'
+
+const bikeTypeMap = {
+  any: 'Любой',
+  road: 'Шоссе',
+  cx: 'CX',
+  mtb: 'MTB'
+}
 
 @Component({
   selector: 'app-add-workout-page',
@@ -10,10 +17,41 @@ import { combineLatest } from 'rxjs';
   styleUrls: [ './add-workout-page.component.scss' ]
 })
 export class AddWorkoutPageComponent implements OnInit {
-  isLinear = true;
-  public routeFormGroup: FormGroup;
-  public bikeTypeFormGroup: FormGroup;
-  public detailsFormGroup: FormGroup;
+  public isLinear = true
+  public routeFormGroup: FormGroup = this.formBuilder.group({
+    from: [ null, Validators.required ],
+    to: [ null, Validators.required ],
+    isCycledRoute: [ false ]
+  })
+
+  public bikeTypeFormGroup: FormGroup = this.formBuilder.group({
+    bikeType: [ null, Validators.required ]
+  })
+
+  public detailsFormGroup: FormGroup = this.formBuilder.group({
+    speed: [ null, Validators.required ],
+    distance: [ null, Validators.required ],
+    duration: [ null ]
+  })
+
+  public workoutSummary = combineLatest([
+    this.routeFormGroup.valueChanges,
+    this.bikeTypeFormGroup.valueChanges,
+    this.detailsFormGroup.valueChanges
+  ]).pipe(
+    tap(([ route, type, details ]) => {
+      this.workout = {
+        workoutType: 'rc',
+        date: new Date().toISOString(),
+        routePoints: route.isCycledRoute ? [ route.from, route.to, route.from ] : [ route.from, route.to ],
+        oneWayRoute: false,
+        distance: details.distance,
+        speed: details.speed,
+        bikeType: bikeTypeMap[ type.bikeType ],
+        members: [],
+      }
+    })
+  )
   /**
    * TODO Сделать проверку в компоненте с блоком информации по тренировке на отсутствие данных, чтобы не мокать ничего заранее
    */
@@ -26,43 +64,17 @@ export class AddWorkoutPageComponent implements OnInit {
     speed: 0,
     bikeType: 'Любой',
     members: [],
-  };
+  }
 
   constructor(private formBuilder: FormBuilder) {
   }
 
-  ngOnInit(): void {
-    this.routeFormGroup = this.formBuilder.group({
-      from: [ null, Validators.required ],
-      to: [ null, Validators.required ]
-    });
-    this.bikeTypeFormGroup = this.formBuilder.group({
-      bikeType: [ 'any' ]
-    });
-    this.detailsFormGroup = this.formBuilder.group({
-      speed: [ null, Validators.required ],
-      distance: [ null, Validators.required ],
-      duration: [ null ]
-    });
+  public ngOnInit(): void {
+    this.workoutSummary.subscribe()
+  }
 
-    combineLatest([
-      this.routeFormGroup.valueChanges,
-      this.bikeTypeFormGroup.valueChanges,
-      this.detailsFormGroup.valueChanges
-    ]).pipe(
-      tap(([ route, bikeType, details ]) => {
-        this.workout = {
-          workoutType: 'rc',
-          date: new Date().toISOString(),
-          routePoints: [ route.from, route.to ],
-          oneWayRoute: false,
-          distance: details.distance,
-          speed: details.speed,
-          bikeType,
-          members: [],
-        };
-      })
-    ).subscribe();
+  public onSecondStep(): void {
+    this.bikeTypeFormGroup.markAsTouched()
   }
 
   public onSubmit(): void {
