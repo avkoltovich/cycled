b59a58c8a92111e2770530dcd4c4b3ea711c3ffd
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-03-04 22:10:41 +0300

    refactor: дата и тип тренировки вынесены в отдельный компонент

--- src/app/components/workouts-list-page/components/workout-card/workout-card.component.ts
+++ src/app/components/workouts-list-page/components/workout-card/workout-card.component.ts
@@ -1,60 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { PowerZonesDialogComponent } from '../../dialogs/power-zones-dialog/power-zones-dialog.component';
-import { WorkoutCard } from '../../models/models';
-
-const workoutShortTypeMap = {
-  rc: 'R',
-  ed: 'E',
-  tp: 'T',
-  tr: 'T',
-  vo: 'V',
-  an: 'A'
-};
-
-const workoutFullTypeMap = {
-  rc: 'Recovery',
-  ed: 'Endurance',
-  tp: 'Tempo',
-  tr: 'Threshold',
-  vo: 'VO2 Max',
-  an: 'Anaerobic'
-}
+import { Component, Input } from '@angular/core'
+import { WorkoutCard } from '../../models/models'
 
 @Component({
   selector: 'app-workout-card',
   templateUrl: './workout-card.component.html',
   styleUrls: [ './workout-card.component.scss' ]
 })
-export class WorkoutCardComponent implements OnInit {
+export class WorkoutCardComponent {
 
   @Input()
   public workout: WorkoutCard
-
-  public workoutTypeCssClass: string
-
-  constructor(public dialog: MatDialog) {
-  }
-
-  ngOnInit(): void {
-    this.workoutTypeCssClass = `workout-card__type--${ this.workout.workoutType }`
-  }
-
-  public getWorkoutShortType(type: string): string {
-    return workoutShortTypeMap[ type ]
-  }
-
-  public getWorkoutFullType(type: string): string {
-    return workoutFullTypeMap[ type ]
-  }
-
-  public getWorkoutTime(dateISO: string): string {
-    const date = new Date(dateISO)
-    return new Intl.DateTimeFormat('ru-RU', { hour: 'numeric', minute: 'numeric' }).format(date)
-  }
-
-  public onPowerZoneBadgeClick(): void {
-    this.dialog.open(PowerZonesDialogComponent)
-  }
 }
 
--- src/app/components/workouts-list-page/components/workout-main-info/workout-main-info.component.ts
+++ src/app/components/workouts-list-page/components/workout-main-info/workout-main-info.component.ts
@@ -1,5 +1,25 @@
 import { Component, Input, OnChanges } from '@angular/core'
 import { WorkoutCard } from '../../models/models'
+import { PowerZonesDialogComponent } from '../../dialogs/power-zones-dialog/power-zones-dialog.component'
+import { MatDialog } from '@angular/material/dialog'
+
+const workoutShortTypeMap = {
+  rc: 'R',
+  ed: 'E',
+  tp: 'T',
+  tr: 'T',
+  vo: 'V',
+  an: 'A'
+}
+
+const workoutFullTypeMap = {
+  rc: 'Recovery',
+  ed: 'Endurance',
+  tp: 'Tempo',
+  tr: 'Threshold',
+  vo: 'VO2 Max',
+  an: 'Anaerobic'
+}
 
 @Component({
   selector: 'app-workout-main-info',
@@ -10,11 +30,12 @@ export class WorkoutMainInfoComponent implements OnChanges {
   public isCardHidden: boolean
   public isSpeedHidden: boolean
   public isDurationHidden: boolean
+  public workoutTypeCssClass: string
 
   @Input()
   public workout: WorkoutCard | null
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
   }
 
   public ngOnChanges(): void {
@@ -23,6 +44,25 @@ export class WorkoutMainInfoComponent implements OnChanges {
     } else {
       this.isSpeedHidden = typeof this.workout.speed === 'undefined' || this.workout.speed === null
       this.isDurationHidden = typeof this.workout.duration === 'undefined' || this.workout.duration === null
+
+      this.workoutTypeCssClass = `workout-card__type--${ this.workout.workoutType }`
     }
+  }
+
+  public getWorkoutShortType(type: string): string {
+    return workoutShortTypeMap[ type ]
+  }
+
+  public getWorkoutFullType(type: string): string {
+    return workoutFullTypeMap[ type ]
+  }
+
+  public getWorkoutTime(dateISO: string): string {
+    const date = new Date(dateISO)
+    return new Intl.DateTimeFormat('ru-RU', { hour: 'numeric', minute: 'numeric' }).format(date)
+  }
+
+  public onPowerZoneBadgeClick(): void {
+    this.dialog.open(PowerZonesDialogComponent)
   }
 }
