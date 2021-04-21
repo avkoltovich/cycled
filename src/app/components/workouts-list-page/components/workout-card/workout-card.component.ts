9ca95c1daae21434cba85732fd18da91a1bd7e1c
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-04-21 23:48:19 +0300

    feat: Возможность делиться тренировкой

--- src/app/components/workouts-list-page/components/workout-card/workout-card.component.ts
+++ src/app/components/workouts-list-page/components/workout-card/workout-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core'
+import { APP_URL } from 'src/shared/constants'
 import { WorkoutModel } from '../../../../models/workout.model'
 
 @Component({
   selector: 'app-workout-card',
   templateUrl: './workout-card.component.html',
-  styleUrls: [ './workout-card.component.scss' ]
+  styleUrls: ['./workout-card.component.scss']
 })
 export class WorkoutCardComponent {
   @Input()
@@ -15,9 +16,13 @@ export class WorkoutCardComponent {
 
   public buildMemberCountString(count: number): string {
     if ((count >= 5 && count <= 19) || (count % 10 >= 5 && count % 10 <= 9) || count % 10 === 0) {
-      return `${ count } участников`
+      return `${count} участников`
     }
 
-    return (count % 10 === 1) ? `${ count } участник` : `${ count } участника`
+    return (count % 10 === 1) ? `${count} участник` : `${count} участника`
+  }
+
+  public onShareButtonClick(id: string): void {
+    window.open(`${APP_URL}/workout/${id}`, '_blank')
   }
 }
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,2 @@
 export const API_URL = 'https://cycled-krd.herokuapp.com/api'
+export const APP_URL = 'https://cycled-krd.web.app'
