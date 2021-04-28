0fe8855727852d112eba16251b280f0b0f2bf1e1
Aleksandr Koltovich <avkoltovich@gmail.com>
2021-04-29 00:13:46 +0300

    fix: Путь до API

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://192.168.1.181:8080/api'
+export const API_URL = 'https://cycled-krd.herokuapp.com/api'
 export const APP_URL = 'https://cycled-krd.web.app'
 
 export const JWT_TOKEN = 'JWT_TOKEN'
