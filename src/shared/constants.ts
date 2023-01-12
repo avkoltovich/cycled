d8b1a6fb0374083faf9039b0b6a5d114e5891ff3
Aleksandr Koltovich <avkoltovich@gmail.com>
2023-01-12 23:10:36 +0300

    feat: адрес Api

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://fill.unjolteonan.com/api'
+export const API_URL = 'https://cycled-krd.herokuapp.com/api'
 export const APP_URL = 'https://cycled-krd.web.app'
 
 export const JWT_TOKEN = 'JWT_TOKEN'
